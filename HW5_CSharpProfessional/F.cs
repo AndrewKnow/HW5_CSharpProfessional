c3feab977770391730ead3af2cff06791dc03b31
Андрей Наумов <NaumovAV1983@yandex.ru>
2023-06-19 14:40:48 +0300

    Добавьте файлы проекта.

---
+++ HW5_CSharpProfessional/F.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5_CSharpProfessional
+{
+    internal class F
+    {
+        int i1;
+        int i2;
+        int i3;
+        int i4;
+        int i5;
+
+        F Get() => new F() { i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5 };     
+    }
+}
